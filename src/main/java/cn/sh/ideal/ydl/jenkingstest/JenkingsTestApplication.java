11beaf4baed8c0407f79c88edbb7ad5005e980c8
yindalei <yindalei00@qq.com>
2019-08-09 10:58:47 +0800

    aaaaaa

---
+++ src/main/java/cn/sh/ideal/ydl/jenkingstest/JenkingsTestApplication.java
@@ -0,0 +1,25 @@
+package cn.sh.ideal.ydl.jenkingstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkingsTestApplication {
+
+public static void main(String[] args) {
+    SpringApplication.run(JenkingsTestApplication.class, args);
+}
+
+@PostMapping("/")
+public String a() {
+
+
+    return "success";
+
+}
+
+}
